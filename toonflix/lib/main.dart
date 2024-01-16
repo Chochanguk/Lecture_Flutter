65e7725a5053759bd2e936bcf634b7ebfcd42cd4
changuk2 <changuk0308@naver.com>
2024-01-16 20:07:11 +0900

    Lifecycle (build context,init state, dispose)

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -18,60 +18,84 @@ class App extends StatefulWidget {
 
 //State는 우리가 UI를 구축하고,데이터를 저장하는곳
 class _AppState extends State<App> {
-  /*
-  int counter = 0; //저장할 데이터(클래스의 특성)
-  void onClicked() {
-    //★★★★setState함수는 State 클래스에 데이터의 변경을 알리는 함수이다.★★★
-    //이것은 새로운 데이터 와 함께 build를 한번더 하는 방법으로 실행된다.
+  bool showTitle = true;
+  //버튼 클릭시 텍스트를 지웠다가 나타나게 하는 메소드 생성(dispose 메소드를 설명하기 위함.)
+  void toggleTitle() {
     setState(() {
-      counter += 1;
-    });
-    /*혹은 밑과 같이 사용가능. 추가적으로 이 주석을 지우면 두개씩 증가함.
-    //한번더 더하고 빌드+더하고 마지막 빌드->+2
-    counter += 1;
-    setState(() {});
-    */
-    */
-  List<int> numbers = [];
-
-  void onClicked() {
-    setState(() {
-      numbers.add(numbers.length);
+      showTitle = !showTitle;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //테마의 데이터중 텍스트에서 큰 제목 타이틀의 색상을 레드로 지정.
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(fontSize: 30),
-              ),
-              /*
-              Text(
-                '$counter',
-                style: const TextStyle(fontSize: 30),
-              ),
-              */
-              //★★Collection for 기능★★
-              //numbers안의 요소에 각 반복 실행마다 1:1 대응
-              for (var n in numbers) Text('$n'),
-              //IconButton위젯은 onPressed: 클릭될때 액셜될 지정함수,
-              //icon: const Icon(Icons.~~)로 구성+(기타 등등 EX)iconSize)
+              //내가 지정한 위젯을 사용
+              showTitle ? const MyLargeTitle() : const Text('Nothing'),
               IconButton(
-                iconSize: 40,
-                onPressed: onClicked,
-                icon: const Icon(Icons.add_box_rounded),
-              ),
+                onPressed: toggleTitle,
+                icon: const Icon(Icons.remove_red_eye),
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+//Extract Widget: 하나의 메소드로 분류해서 처리하게 함.
+//이는 StatelssWidget으로 처리하는 경우는 UI만 다룸.
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  //값을 initia시켜주는 메소드(build전에 실행함.)
+  //보통 API나 부모요소에 의존하는 데이터를 초기화해야 하는 경우,API에서 업데이트를 구독할때 사용
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  //dispose메소드는 위젯이 스크린에서 제거될 때 호출되는 메서드이다.
+  //위에선  MyLargeTitle()의 텍스트가 사라지면 실행됨.
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose!");
+  }
+
+//BuildContext context: 이전에 있는 모든 상위 요소들에 대한 정보이다.
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        //★★★context를 통해 상위 요소의 값을 불러올 수 있다.★★★
+        //해석: color는 상위요소의 Theme인데, textTheme의 titleLage의 color이다.
+        //이때 titleLarge의 색은 Null이면 안되므로 ?를 붙여준다.
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
